c65aa7ab78ec02986d683c98c9423824b7d8f529
Urffsamhunt <iwriterofficial511@gmail.com>
2021-09-19 23:43:55 +0530

    huehue

--- FascNum.java
+++ FascNum.java
@@ -1,4 +1,3 @@
-package ProJava;
 
 import java.util.Scanner;
 
--- MatchAnagram.java
+++ MatchAnagram.java
@@ -7,7 +7,6 @@ public class MatchAnagram {
         System.out.println("Enter the two strings ");
         String s1 = scanner.next().toLowerCase();
         String s2 = scanner.next().toLowerCase();
-        MatchAnagram m = new RegExp(s2, "g");
         if (s1.replace("[s2]","").length() == 0 )
         System.out.println("The Two Strings are Anagrams");
         else System.out.println("The Two Strings are not anagrams");
