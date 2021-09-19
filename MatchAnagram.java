df507dce8a2e04ffa7abf6c89fb0c7a7a1daf279
Urffsamhunt <iwriterofficial511@gmail.com>
2021-09-19 23:53:35 +0530

    Update

--- MatchAnagram.java
+++ MatchAnagram.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.*;
 
 public class MatchAnagram {
     public static void main(String[] args) {
@@ -7,10 +6,23 @@ public class MatchAnagram {
         System.out.println("Enter the two strings ");
         String s1 = scanner.next().toLowerCase();
         String s2 = scanner.next().toLowerCase();
-        if (s1.replace("[s2]","").length() == 0 )
-        System.out.println("The Two Strings are Anagrams");
-        else System.out.println("The Two Strings are not anagrams");
-        String re = "\\b"+s2+"\\b";
-        System.out.println(s1.replace(re,""));
+        if (frequency(s1, s2))
+        System.out.println("They are anagrams");
+        else
+        System.out.println("They are not Anagrams");
+        scanner.close();
     }
+
+    static boolean frequency(String s, String s1)
+    {
+        while (s.length() > 0)
+        {
+            String m = ""+s.charAt(0);
+            String n = s.replaceAll(m,"");
+            String n1 = s1.replaceAll(m,"");
+            if(s.length()-n.length() != s1.length()-n1.length())
+            return false;
+            s =n;
+        }return true;
+    }   
 }
